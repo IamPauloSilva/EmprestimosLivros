f94476db1f2a71f86e4a0eaa57cb18d86ba54aa8
Paulo Silva <timmylaggy.as@gmail.com>
2024-07-21 18:40:18 +0100

    Criacao do projeto

---
+++ EmprestimosLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EmprestimosLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Emprestador { get; set; }
+
+        public string Recebedor { get; set; }
+        public string NomeLivro { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+
+    }
+
+}
