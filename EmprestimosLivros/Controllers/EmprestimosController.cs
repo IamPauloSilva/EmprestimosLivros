870fcb23844d347087cd6c55f525d855e12b5d26
Paulo Silva <timmylaggy.as@gmail.com>
2024-07-21 20:53:23 +0100

    criacao da view e tabela

--- EmprestimosLivros/Controllers/EmprestimosController.cs
+++ EmprestimosLivros/Controllers/EmprestimosController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmprestimosLivros.Data;
+using EmprestimosLivros.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmprestimosLivros.Controllers
 {
     public class EmprestimosController : Controller
     {
+        readonly private ApplicationDbContext _db;
+        public EmprestimosController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
         }
     }
 }
