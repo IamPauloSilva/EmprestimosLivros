661e24b614444a56bb31ddb25e931651c4239473
Paulo Silva <timmylaggy.as@gmail.com>
2024-07-21 22:00:54 +0100

    adicionada view criar funcional

--- EmprestimosLivros/Controllers/EmprestimosController.cs
+++ EmprestimosLivros/Controllers/EmprestimosController.cs
@@ -16,5 +16,22 @@ namespace EmprestimosLivros.Controllers
             IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
             return View(emprestimos);
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
