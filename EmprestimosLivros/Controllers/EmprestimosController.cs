3abec4b1cb5b050ed266a1a9f5df95b11574832a
Paulo Silva <timmylaggy.as@gmail.com>
2024-07-21 23:28:16 +0100

    adicionadas funcionalidades editar e excluir

--- EmprestimosLivros/Controllers/EmprestimosController.cs
+++ EmprestimosLivros/Controllers/EmprestimosController.cs
@@ -33,5 +33,61 @@ namespace EmprestimosLivros.Controllers
             }
             return View();
         }
+
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            } 
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo) 
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(emprestimo);
+        }
+
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- EmprestimosLivros/Models/EmprestimosModel.cs
+++ EmprestimosLivros/Models/EmprestimosModel.cs
@@ -1,11 +1,18 @@
-﻿namespace EmprestimosLivros.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimosLivros.Models
 {
     public class EmprestimosModel
     {
         public int Id { get; set; }
+
+        [Required (ErrorMessage ="Digite o nome do Fornecedor")]
         public string Emprestador { get; set; }
 
+        [Required(ErrorMessage = "Digite o nome do Recebedor")]
         public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Livro")]
         public string NomeLivro { get; set; }
         public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
 
